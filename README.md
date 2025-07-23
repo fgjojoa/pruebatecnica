070a17c6022b9c27371742070bdd267bcda36f37
Ferney Duran <fduran@asiven.com>
2025-07-23 18:41:12 -0500

    Primer Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Documentación Prueba Tecnica
---
+++ src/main/java/com/linktic/pruebatecnica/PruebaLinkTicApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.pruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaLinkTicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaLinkTicApplication.class, args);
+	}
+
+}
